d02165332731cf40b5ec068d07e2603079726a26
Priyanshu kumar <priyanshu12656@gmail.com>
2025-03-07 22:50:01 +0530

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
-import SocialLinks from "./components/SocialLinks";
 import Skills from "./components/Skills";
-import Contact from "./components/Contact";
+import SocialLinks from "./components/SocialLinks";
 
 
 
